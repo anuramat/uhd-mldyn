ae7d4356da0e55e604dfc442d4cda00b6c0819f0
Arsen Nuramatov <anuramat@pm.me>
2024-12-23 15:43:57 +0100

    feat(deps): move pip folder to cache

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           shellHook = ''
             # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
             # See https://pip.pypa.io/en/stable/topics/configuration/
-            export PIP_PREFIX="$(pwd)/_build/pip_packages"
+            export PIP_PREFIX=$(realpath -m "''${XDG_CACHE_HOME:-$HOME/.cache}/flake_pip/$(pwd)/")
             export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
             export PATH="$PIP_PREFIX/bin:$PATH"
             unset SOURCE_DATE_EPOCH
